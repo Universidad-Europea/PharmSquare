c91dc96540d35321c38b9c6554ddbed5cd543851
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-28 21:42:12 +0100

    terminal lista y tabulación añadida

--- database/creacion_db.sql
+++ database/creacion_db.sql
@@ -1,64 +1,54 @@
 CREATE TABLE CLIENTE (
-	dni VARCHAR2(9) CONSTRAINT CLIENTE_DNI_PK PRIMARY KEY,
-	fecha_alta DATE,
-	direccion VARCHAR2(50),
-	nacimiento DATE,
-	contrasena VARCHAR2(50),
-	sexo VARCHAR2(1),
-	telefono VARCHAR2(9),
-	mail VARCHAR2(50),
+	dni 		VARCHAR2(9) CONSTRAINT CLIENTE_DNI_PK PRIMARY KEY,
+	fecha_alta 	DATE,
+	direccion 	VARCHAR2(50),
+	nacimiento 	DATE,
+	contrasena 	VARCHAR2(50),
+	sexo 		VARCHAR2(1),
+	telefono 	VARCHAR2(9),
+	mail 		VARCHAR2(50),
 
 	CONSTRAINT CLIENTE_SEXO_CHK CHECK sexo in ('H', 'M', 'O')
 );
 
 CREATE TABLE HISTORIAL (
-	id NUMBER(5) CONSTRAINT HISTORIAL_ID_PK PRIMARY KEY,
-	fecha DATE,
-	dni VARCHAR2(9),
-	diagnostico VARCHAR2(300),
+	id			NUMBER(5) CONSTRAINT HISTORIAL_ID_PK PRIMARY KEY,
+	fecha		DATE,
+	dni			VARCHAR2(9),
+	diagnostico	VARCHAR2(300),
 
 	CONSTRAINT HISTORIAL_DNI_FK FOREIGN KEY (dni) REFERENCES CLIENTE(dni)
 );
 
 CREATE TABLE PRODUCTOS (
-	id NUMBER(5) CONSTRAINT PRODUCTO_ID_PK PRIMARY KEY,
-	utilidad VARCHAR2(50),
-	nombre VARCHAR2(50),
-	laboratorio VARCHAR2(50),
-	precio NUMBER(5,2),
-	stock NUMBER(3),
-	foto VARCHAR2(150),
-	necesita_login VARCHAR2(2),
-	-- CATEGORIA
+	id				NUMBER(5) CONSTRAINT PRODUCTO_ID_PK PRIMARY KEY,
+	utilidad		VARCHAR2(50),
+	nombre			VARCHAR2(50),
+	laboratorio		VARCHAR2(50),
+	precio			NUMBER(5,2),
+	stock			NUMBER(3),
+	foto			VARCHAR2(150),
+	necesita_login	VARCHAR2(2),
+	-- ! CATEGORIA
 
 	CONSTRAINT PROD_NECE_LOG_CHK CHECK necesita_login IN ('SI', 'NO')
 );
 
--- #### terminal pago:
--- - moneda_1c
--- - moneda_2c
--- - moneda_5c
--- - moneda_10c
--- - moneda_20c
--- - moneda_50c
--- - moneda_1e
--- - billete_5e
--- - billete_10e
--- - billete_50e
--- - billete_100e
 
-CREATE TABLE terminal_pago (
-	moneda_1c INTEGER,
-	moneda_2c INTEGER,
-	moneda_5c INTEGER,
-	moneda_10c INTEGER,
-	moneda_20c INTEGER,
-	moneda_50c INTEGER,
-	moneda_1e INTEGER,
-	billete_5e INTEGER,
-	billete_10e INTEGER,
-	billete_50e INTEGER,
-	billete_100e INTEGER
+
+CREATE TABLE TERMINAL (
+	id				NUMBER(1) CONSTRAINT TERMINAL_ID_PK PRIMARY KEY,
+	moneda_1c		NUMBER(2) DEFAULT 0,
+	moneda_2c		NUMBER(2) DEFAULT 0,
+	moneda_5c		NUMBER(2) DEFAULT 0,
+	moneda_10c		NUMBER(2) DEFAULT 0,
+	moneda_20c		NUMBER(2) DEFAULT 0,
+	moneda_50c		NUMBER(2) DEFAULT 0,
+	moneda_1e		NUMBER(2) DEFAULT 0,
+	billete_5e		NUMBER(2) DEFAULT 0,
+	billete_10e		NUMBER(2) DEFAULT 0,
+	billete_50e		NUMBER(2) DEFAULT 0,
+	billete_100e	NUMBER(2) DEFAULT 0
 );
 
 
