6e04f0239da0b72c8406d6ad4130e493f110ea94
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-01 11:34:12 +0200

    Main created

---
+++ src/main/java/dam/pharmaSquare/main/Main.java
@@ -0,0 +1,29 @@
+package dam.pharmaSquare.main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+//        try {
+//            UIManager.setLookAndFeel(new FlatDarculaLaf());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Starting application");
+//                View window = new View();
+
+//                PharmaDB db = new PharmaDB();
+
+//                Controller controller = new Controller(
+//                    db
+//                );
+//                window.setController(controller);
+//                window.setVisible(true);
+            }
+        });
+    }
+}
