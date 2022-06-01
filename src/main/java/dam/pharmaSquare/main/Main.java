afc47c1cfcc6d7c81bf2b45bac17f2cd5a804cb5
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-01 11:36:42 +0200

    Theme added

--- src/main/java/dam/pharmaSquare/main/Main.java
+++ src/main/java/dam/pharmaSquare/main/Main.java
@@ -1,15 +1,17 @@
 package dam.pharmaSquare.main;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-//        try {
-//            UIManager.setLookAndFeel(new FlatDarculaLaf());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        try {
+            UIManager.setLookAndFeel(new FlatDarculaLaf());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
