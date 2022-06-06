68aebcac2c5d320aae422331047b6504aa7b9ac0
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-06 10:43:50 +0200

    personal and transaccion also added

---
+++ src/main/java/dam/pharmaSquare/model/persistencia/PPersonal.java
@@ -0,0 +1,11 @@
+package dam.pharmaSquare.model.persistencia;
+
+public class PPersonal {
+    public static final String DNI = "DNI";
+    public static final String NOMBRE = "NOMBRE";
+    public static final String CATEGORIA = "CATEGORIA";
+    public static final String PASSWD = "PASSWD";
+
+    public static final String ID = "DNI";
+    public static final String TABLE_NAME = "PERSONAL";
+}
---
+++ src/main/java/dam/pharmaSquare/model/persistencia/PTransaccion.java
@@ -0,0 +1,11 @@
+package dam.pharmaSquare.model.persistencia;
+
+public class PTransaccion {
+    public static final String DNI = "DNI_CLIENTE";
+    public static final String ID_PRODUCTO = "PRODUCTO_ID";
+    public static final String FECHA = "FECHA";
+    public static final String CANTIDAD = "CANTIDAD";
+
+    public static final String[] ID = {DNI, ID_PRODUCTO, FECHA};
+    public static final String TABLE_NAME = "CLIENTE";
+}
