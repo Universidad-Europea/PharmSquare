c91fc6875453646684c2016d7138a68beaf29a49
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-06 14:58:07 +0200

    CategoriasProductos added

--- src/main/java/dam/pharmaSquare/model/CategoriaProducto.java
+++ src/main/java/dam/pharmaSquare/model/CategoriaProducto.java
@@ -1,6 +1,26 @@
 package dam.pharmaSquare.model;
 
+import dam.dataValidation.DataValidation;
+import dam.exception.InvalidDataException;
+
 public class CategoriaProducto {
-    private String id;
+    private Integer id;
     private String nombre;
+
+    public CategoriaProducto(Integer id, String nombre) {
+        this.setId(id);
+        this.setNombre(nombre);
+    }
+
+    private void setId(Integer id) {
+        if (!DataValidation.isNatural(id))
+            throw new InvalidDataException("El id no es un natural.");
+        this.id = id;
+    }
+
+    private void setNombre(String nombre) {
+        if (!DataValidation.isStringValid(nombre))
+            throw new InvalidDataException("El nombre de la categoría del producto no es válida.");
+        this.nombre = nombre;
+    }
 }
--- src/main/java/dam/pharmaSquare/model/CategoriasProductos.java
+++ src/main/java/dam/pharmaSquare/model/CategoriasProductos.java
@@ -1,6 +1,23 @@
 package dam.pharmaSquare.model;
 
+import java.util.ArrayList;
+
 public class CategoriasProductos {
-    private String id_producto;
-    private String id_categoria;
+    private Producto producto;
+    private ArrayList<CategoriaProducto> categorias;
+
+    public CategoriasProductos(Producto producto, ArrayList<CategoriaProducto> categorias) {
+        this.producto = producto;
+        this.categorias = categorias;
+    }
+
+    // GETTERS
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public ArrayList<CategoriaProducto> getCategorias() {
+        return categorias;
+    }
 }
