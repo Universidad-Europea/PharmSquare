07d435aa5b52f25b00a85872baa8c641bc9987b4
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-06 15:28:19 +0200

    Validation of phone and mail added

--- src/main/java/dam/dataValidation/DataValidation.java
+++ src/main/java/dam/dataValidation/DataValidation.java
@@ -3,9 +3,23 @@ package dam.dataValidation;
 public class DataValidation {
     private static final String DNI_REGEX = "^\\d{7,9}[A-Z]$";
     private static final String NOMBRE_REGEX = "^[A-Za-z]+ ?[A-Za-z]+ ?[A-Za-z]+$";
+    private static final String PHONE_REGEX = "^(\\+\\d{2,3})? ?\\d{3} ?(\\d{3} ?\\d{3}|\\d{2} ?\\d{2} ?\\d{2})$";
+    private static final String MAIL_REGEX = "^[a-z][a-z1-9._-]*@[a-z]+\\.[a-z]{1,3}$";
 
     public static boolean isDniValid(String dni) {
         if (dni == null || !dni.matches(DNI_REGEX))
+            return false;
+        return true;
+    }
+
+    public static boolean isPhoneValid(String phone) {
+        if (phone == null || !phone.matches(PHONE_REGEX))
+            return false;
+        return true;
+    }
+
+    public static boolean isMailValid(String mail) {
+        if (mail == null || !mail.matches(MAIL_REGEX))
             return false;
         return true;
     }
