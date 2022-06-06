6400ff71e84c4136172d6cd2c14d67f08d6a2194
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-06 22:12:09 +0200

    exception classes commented

--- src/main/java/dam/exception/SQLiteQueryException.java
+++ src/main/java/dam/exception/SQLiteQueryException.java
@@ -1,5 +1,8 @@
 package dam.exception;
 
+/**
+ * Clase para excepciones relacionadas con SQLiteQuery.
+ */
 public class SQLiteQueryException extends InvalidDataException{
     public SQLiteQueryException(String s) {
         super(s);
