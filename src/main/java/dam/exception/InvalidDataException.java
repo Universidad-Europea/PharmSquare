66cc71036e16dff48d539ff071ccc8a7d1426717
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-06 14:42:40 +0200

    Updated to be able to throw an exception with an array inside the message

--- src/main/java/dam/exception/InvalidDataException.java
+++ src/main/java/dam/exception/InvalidDataException.java
@@ -10,4 +10,21 @@ public class InvalidDataException extends RuntimeException {
 	public InvalidDataException(String s, int min, int max) {
 		super(String.format("%s [%d, %d]", s, min, max));
 	}
+
+	public InvalidDataException(String s, Object[] arr) {
+		super(addArrayToStr(s, arr));
+	}
+
+	private static String addArrayToStr(String s, Object[] arr) {
+		String arrStr = "{";
+		if (arr != null && arr.length > 0) {
+			arrStr += " " + arr[0];
+			for (int i = 1; i < arr.length; i++) {
+				arrStr += ", " + arr[i];
+			}
+		}
+		arrStr += "}";
+
+		return String.format(s, arrStr);
+	}
 }
