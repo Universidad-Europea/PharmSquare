e9f2ec1f0a4f8fc4e5e7537e86f972218fdc893c
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-06 11:04:07 +0200

    ViewInicio basic UI implemented

--- src/main/java/dam/pharmaSquare/view/inicio/ViewInicio.java
+++ src/main/java/dam/pharmaSquare/view/inicio/ViewInicio.java
@@ -1,4 +1,17 @@
 package dam.pharmaSquare.view.inicio;
 
+import javax.swing.*;
+
 public class ViewInicio {
+    private JPanel jpBody;
+    private JPanel jpForm;
+    private JPanel jpTopElements;
+    private JLabel lblTime;
+    private JButton btnStaff;
+    private JLabel lblTitle;
+    private JTextField txtfMail;
+    private JPasswordField passwfPassword;
+    private JButton btnSubmmit;
+    private JButton btnNewCliente;
+    private JButton btnNoLogin;
 }
