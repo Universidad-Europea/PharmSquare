ad39bde57e86b782712a04f3b2efa79e01579ded
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-06 16:07:26 +0200

    dni and id producto added

--- src/main/java/dam/pharmaSquare/model/Transaccion.java
+++ src/main/java/dam/pharmaSquare/model/Transaccion.java
@@ -1,8 +1,29 @@
 package dam.pharmaSquare.model;
 
+import dam.dataValidation.DataValidation;
+
 public class Transaccion {
     private String dni;
-    private String id_producto;
+    private int idProducto;
     private String fecha; // DateTime
     private String cantidad;
+
+    public Transaccion(String dni, int idProducto, String fecha, String cantidad) {
+        this.setDni(dni);
+        this.setIdProducto(idProducto);
+        this.setFecha(fecha);
+        this.setCantidad(cantidad);
+    }
+
+    public void setDni(String dni) {
+        if (!DataValidation.isDniValid(dni))
+            throw new IllegalArgumentException("El DNI no es válido.");
+        this.dni = dni;
+    }
+
+    public void setIdProducto(int idProducto) {
+        if (!DataValidation.isNatural(idProducto))
+            throw new IllegalArgumentException("El ID del producto no es válido.");
+        this.idProducto = idProducto;
+    }
 }
