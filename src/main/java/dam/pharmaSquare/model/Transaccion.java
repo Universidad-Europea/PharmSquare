ea01948c6897ac1f74cf62a05a3ff561266c21e8
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-06 17:48:54 +0200

    transaction added

--- src/main/java/dam/pharmaSquare/model/Transaccion.java
+++ src/main/java/dam/pharmaSquare/model/Transaccion.java
@@ -6,9 +6,9 @@ public class Transaccion {
     private String dni;
     private int idProducto;
     private String fecha; // DateTime
-    private String cantidad;
+    private double cantidad;
 
-    public Transaccion(String dni, int idProducto, String fecha, String cantidad) {
+    public Transaccion(String dni, int idProducto, String fecha, double cantidad) {
         this.setDni(dni);
         this.setIdProducto(idProducto);
         this.setFecha(fecha);
@@ -25,5 +25,16 @@ public class Transaccion {
         if (!DataValidation.isNatural(idProducto))
             throw new IllegalArgumentException("El ID del producto no es válido.");
         this.idProducto = idProducto;
+    }
+
+    public void setFecha(String fecha) {
+        // TODO Validar fecha
+        this.fecha = fecha;
+    }
+
+    public void setCantidad(double cantidad) {
+        if (!DataValidation.isPositive(cantidad))
+            throw new IllegalArgumentException("La cantidad no es válida.");
+        this.cantidad = cantidad;
     }
 }
