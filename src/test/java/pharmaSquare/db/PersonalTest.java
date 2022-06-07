0211deca3a0cad0f58f634ce22eb2a631343a8c9
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-07 22:24:31 +0200

    now true test

--- src/test/java/pharmaSquare/db/PersonalTest.java
+++ src/test/java/pharmaSquare/db/PersonalTest.java
@@ -3,12 +3,21 @@ package pharmaSquare.db;
 import dam.pharmaSquare.db.PharmaSquareDB;
 import dam.pharmaSquare.model.Personal;
 
+import org.junit.*;
+
 import java.util.ArrayList;
 
 public class PersonalTest {
-    public static void main(String[] args) {
-        PharmaSquareDB db = new PharmaSquareDB();
+    private static PharmaSquareDB db;
 
+    @BeforeClass
+    public static void setUpBeforeClass() {
+        db = new PharmaSquareDB();
+    }
+
+
+    @Test
+    public void getPersonalTest() {
         for (String type : PharmaSquareDB.PERSONAL_FILTERS) {
             System.out.println("--------------------------");
             System.out.println(type);
@@ -16,7 +25,6 @@ public class PersonalTest {
             System.out.println("--------------------------");
         }
     }
-
 
     private static void printPersonal(ArrayList<Personal> personal) {
         for (Personal p : personal) {
