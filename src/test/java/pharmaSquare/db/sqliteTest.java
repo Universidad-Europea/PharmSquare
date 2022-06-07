988941b5d4378b4a4cd5d25299847152d3961312
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-07 20:48:15 +0200

    Able to open db

---
+++ src/test/java/pharmaSquare/db/sqliteTest.java
@@ -0,0 +1,11 @@
+package pharmaSquare.db;
+
+
+import dam.pharmaSquare.db.PharmaSquareDB;
+
+public class sqliteTest {
+    public static void main(String[] args) {
+        PharmaSquareDB db = new PharmaSquareDB();
+    }
+
+}
