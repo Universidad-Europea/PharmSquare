7ede637af8f58c56a018cc17f7df41796b91c2a2
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-08 15:31:53 +0200

    basic test for transactions added

---
+++ src/test/java/pharmaSquare/db/TransaccionTest.java
@@ -0,0 +1,47 @@
+package pharmaSquare.db;
+
+import dam.pharmaSquare.db.PharmaSquareDB;
+import dam.pharmaSquare.model.Personal;
+import dam.pharmaSquare.model.Transaccion;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TransaccionTest {
+    private static PharmaSquareDB db;
+
+    private static final Object[][] tests = {
+        {null, null, true},
+        {null, null, true}
+    };
+
+    @BeforeClass
+    public static void setUpBeforeClass() {
+        db = new PharmaSquareDB();
+    }
+
+
+    @Test
+    public void getPersonalTest() {
+        for (Object[] r : tests) {
+            System.out.println("--------------------------");
+            System.out.println("--------------------------");
+            System.out.println("-----                -----");
+            printTransacciones(db.getTransacciones((String) r[0], (String) r[1], (boolean) r[2]));
+            System.out.println("-----                -----");
+            System.out.println("--------------------------");
+            System.out.println("--------------------------");
+        }
+    }
+
+    // TOOLS
+    private static void printTransacciones(ArrayList<Transaccion> personal) {
+        for (Transaccion t : personal) {
+            System.out.println(t);
+        }
+    }
+}
