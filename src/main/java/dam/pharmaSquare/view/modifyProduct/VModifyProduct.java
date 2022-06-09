209c71cad2d4e87080e7e715e4f1ea848b0ca328
mariadv_ <95478604+mariaduva@users.noreply.github.com>
2022-06-09 17:39:52 +0200

    finished design of consult and modify products windows

--- src/main/java/dam/pharmaSquare/view/modifyProduct/VModifyProduct.java
+++ src/main/java/dam/pharmaSquare/view/modifyProduct/VModifyProduct.java
@@ -8,4 +8,22 @@ public class VModifyProduct {
     private JButton btnBack;
     private JButton btnClock;
     private JLabel lblTitle;
+    private JPanel jpForm;
+    private JButton btnSave;
+    private JLabel lblIdProduct;
+    private JTextField txtProdNm;
+    private JLabel lblName;
+    private JSpinner spnPrice;
+    private JLabel lblPrice;
+    private JSpinner spnStock;
+    private JLabel lblStock;
+    private JComboBox cmbCategory;
+    private JLabel lblCategory;
+    private JLabel lblLab;
+    private JTextField txtLab;
+    private JTextPane txtUtility;
+    private JRadioButton rdbYes;
+    private JRadioButton rdbNo;
+    private JLabel lblLogin;
+    private JLabel lblUtility;
 }
