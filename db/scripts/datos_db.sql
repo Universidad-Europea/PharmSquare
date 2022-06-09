e2a175ae7038898cb6a8c232dfacaf068305c435
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-09 22:45:16 +0200

    DB updated with latest changes

--- db/scripts/datos_db.sql
+++ db/scripts/datos_db.sql
@@ -9,61 +9,61 @@ INSERT INTO CLIENTE
 VALUES ('000000000A', 'no registrado', NULL, NULL, NULL, NULL, 'O', NULL, NULL);
 
 INSERT INTO CLIENTE
-VALUES ('123456789A', 'Juan', '2019-01-01', 'C/Calle m nº 1', '1990-01-01', '12ab34!!', 'H', '666666666', 'juan@gmail.com');
+VALUES ('123456789A', 'Juan Garcia', '2019-01-01', 'C/Calle m nº 1', '1995-02-01', '12a!DSAb34!', 'H', '635 456 789', 'juan@gmail.com');
 
 INSERT INTO CLIENTE
-VALUES ('987654321A', 'Pedro', '2019-01-01', 'C/Calle m nº 2', '1990-01-01', '12ab34!!', 'H', '666666666', 'pedro@gmail.com');
+VALUES ('987654321A', 'Pedro Perez', '2019-01-01', 'C/Calle m nº 2', '1980-01-04', '@@134daSA', 'H', '635 45 67 43', 'pedro@gmail.com');
 
 INSERT INTO CLIENTE
-VALUES ('123456789B', 'Maria', '2019-01-01', 'C/Calle m nº 3', '1990-01-01', '12ab34!!', 'M', '666666666', 'maria@gmail.com');
+VALUES ('123456789B', 'Maria Perez', '2019-01-01', 'C/Calle m nº 3', '1970-05-08', '12abfCS!!', 'M', '735456789', 'maria@gmail.com');
 
 INSERT INTO CLIENTE
-VALUES ('987654321B', 'Ana', '2019-01-01', 'C/Calle m nº 4', '1990-01-01', '12ab34!!', 'M', '666666666', 'ana@gmail.com');
+VALUES ('987654321B', 'Ana Lopez', '2019-01-01', 'C/Calle m nº 4', '1999-07-04', '34!FDSsa!', 'M', '743545359', 'ana@gmail.com');
 
 
 -- Rellenar PRODUCTO
 
 INSERT INTO PRODUCTO (nombre, utilidad, laboratorio, precio, stock, foto, necesita_login)
-VALUES ('Bendaje de piel', 'Utilidad 1', 'Laboratorio 1', 9.99, 10, './res/img/productos/bendaje.jpg', 'S');
+VALUES ('Vendaje de piel', 'Vendaje de 30 unidades para la piel.', 'Jhonson & Johnson', 8.69, 10, './res/img/productos/bendaje.jpg', 'S');
 
 INSERT INTO PRODUCTO (nombre, utilidad, laboratorio, precio, stock, foto, necesita_login)
-VALUES ('Ibuprofeno', 'Utilidad 2', 'Laboratorio 1', 9.99, 10, './res/img/productos/ibuprofeno.jpg', 'S');
+VALUES ('Ibuprofeno', 'Ibuprofeno 600mg.', 'Jhonson & Johnson', 5.99, 10, './res/img/productos/ibuprofeno.jpg', 'S');
 
 INSERT INTO PRODUCTO (nombre, utilidad, laboratorio, precio, stock, foto, necesita_login)
-VALUES ('Paracetamol', 'Utilidad 3', 'Laboratorio 3', 9.99, 10, './res/img/productos/paracetamol.jpg', 'S');
+VALUES ('Paracetamol', 'Paracetamol pack ahorro 40 monodosis.', 'Fharma Labs S.A.', 4.99, 10, './res/img/productos/paracetamol.jpg', 'S');
 
 INSERT INTO PRODUCTO (nombre, utilidad, laboratorio, precio, stock, foto, necesita_login)
-VALUES ('Aspirina', 'Utilidad 4', 'Laboratorio 4', 9.99, 10, './res/img/productos/aspirina.jpg', 'S');
+VALUES ('Aspirina', 'Ahora el doble de burbujas.', 'Fharma Labs S.A.', 3.99, 10, './res/img/productos/aspirina.jpg', 'S');
 
 INSERT INTO PRODUCTO (nombre, utilidad, laboratorio, precio, stock, foto, necesita_login)
-VALUES ('Ebastel', 'Utilidad 5', 'Laboratorio 5', 9.99, 10, './res/img/productos/ebastel.jpg', 'S');
+VALUES ('Ebastel', 'Ebastel monodosis. Pack de 40 dosis.', 'Fharma Labs S.A.', 9.99, 10, './res/img/productos/ebastel.jpg', 'S');
 
 INSERT INTO PRODUCTO (nombre, utilidad, laboratorio, precio, stock, foto, necesita_login)
-VALUES ('Crema solar', 'Utilidad 6', 'Laboratorio 6', 9.99, 10, './res/img/productos/crema_solar.jpg', 'N');
+VALUES ('Crema solar', 'Protección 80.', 'Fharma Labs S.A.', 29.99, 10, './res/img/productos/crema_solar.jpg', 'N');
 
 INSERT INTO PRODUCTO (nombre, utilidad, laboratorio, precio, stock, foto, necesita_login)
-VALUES ('Suero', 'Utilidad 7', 'Laboratorio 5', 9.99, 10, './res/img/productos/suero.jpg', 'S');
+VALUES ('Suero', 'Suero fisiológico', 'Dalsy Corporated', 9.99, 10, './res/img/productos/suero.jpg', 'S');
 
 INSERT INTO PRODUCTO (nombre, utilidad, laboratorio, precio, stock, foto, necesita_login)
-VALUES ('Cafeina', 'Utilidad 8', 'Laboratorio 6', 9.99, 10, './res/img/productos/cafeina.jpg', 'S');
+VALUES ('Cafeina', 'Este producto suele desaparecer de nuestros almacenes por razones misteriosas.', 'Dalsy Corporated', 24.54, 10, './res/img/productos/cafeina.jpg', 'S');
 
 INSERT INTO PRODUCTO (nombre, utilidad, laboratorio, precio, stock, foto, necesita_login)
-VALUES ('Suplemento alimenticio bebes', 'Utilidad 9', 'Laboratorio 6', 9.99, 10, './res/img/productos/suplemento_bebes.jpg', 'N');
+VALUES ('Suplemento alimenticio bebes', 'Sabor crema de frutas y verduras.', 'Dalsy Corporated', 19.99, 10, './res/img/productos/suplemento_bebes.jpg', 'N');
 
 INSERT INTO PRODUCTO (nombre, utilidad, laboratorio, precio, stock, foto, necesita_login)
-VALUES ('Suplemento alimenticio adultos', 'Utilidad 10', 'Laboratorio 2', 9.99, 10, './res/img/productos/suplemento_adultos.jpg', 'N');
+VALUES ('Suplemento alimenticio adultos', 'Nuevo sabor Fabada con natillas.', 'Dalsy Corporated', 19.99, 10, './res/img/productos/suplemento_adultos.jpg', 'N');
 
 INSERT INTO PRODUCTO (nombre, utilidad, laboratorio, precio, stock, foto, necesita_login)
-VALUES ('Suplemento alimenticio niños', 'Utilidad 11', 'Laboratorio 3', 9.99, 10, './res/img/productos/suplemento_ninos.jpg', 'N');
+VALUES ('Suplemento alimenticio niños', 'Sabor vainilla.', 'Dalsy Corporated', 19.99, 10, './res/img/productos/suplemento_ninos.jpg', 'N');
 
 INSERT INTO PRODUCTO (nombre, utilidad, laboratorio, precio, stock, foto, necesita_login)
-VALUES ('Crema hidratante', 'Utilidad 12', 'Laboratorio 4', 9.99, 10, './res/img/productos/crema_hidratante.jpg', 'N');
+VALUES ('Crema hidratante', 'Especialmente diseñada para su absorción cutánea.', 'Dalsy Corporated', 9.99, 10, './res/img/productos/crema_hidratante.jpg', 'N');
 
 INSERT INTO PRODUCTO (nombre, utilidad, laboratorio, precio, stock, foto, necesita_login)
-VALUES ('Tiritas', 'Utilidad 13', 'Laboratorio 5', 9.99, 10, './res/img/productos/tiritas.jpg', 'N');
+VALUES ('Tiritas', 'Apósitos de tamaño reducido.', NULL, 3.99, 10, './res/img/productos/tiritas.jpg', 'N');
 
 INSERT INTO PRODUCTO (nombre, utilidad, laboratorio, precio, stock, foto, necesita_login)
-VALUES ('Apósitos de nitrato de plata', 'Utilidad 14', 'Laboratorio 6', 9.99, 10, './res/img/productos/apositos.jpg', 'N');
+VALUES ('Apósitos de nitrato de plata', 'Tiritas grandes.', NULL, 3.99, 10, './res/img/productos/apositos.jpg', 'N');
 
 
 -- Rellenar CATEGORIA_PRODUCTO
