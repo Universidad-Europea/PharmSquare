c4e86a78da897b30080b808c80b8ef3189c7385d
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-10 22:32:59 +0200

    Test to test all tests in DB added. Man, that msg was confusing hahaha

---
+++ src/test/java/pharmaSquare/db/AllDBTest.java
@@ -0,0 +1,9 @@
+package pharmaSquare.db;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ClienteTest.class, PersonalTest.class, ProductoTest.class, TransaccionTest.class})
+public class AllDBTest {
+}
