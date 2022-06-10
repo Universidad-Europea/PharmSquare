a90b3a2d4592887d601eeb96370ef91e9886d65e
mariadv_ <95478604+mariaduva@users.noreply.github.com>
2022-06-10 02:38:57 +0200

    created VClientsRecords

---
+++ src/main/java/dam/pharmaSquare/view/cliente/VClientsRecords.java
@@ -0,0 +1,18 @@
+package dam.pharmaSquare.view.cliente;
+
+import javax.swing.*;
+
+public class VClientsRecords {
+    private JPanel jpBody;
+    private JPanel jpTopElements;
+    private JButton btnAtrás;
+    private JButton btnClock;
+    private JTextField txtDate;
+    private JComboBox cmbProd;
+    private JTextField txtDniClt;
+    private JButton btnSearch;
+    private JTable tblPurchases;
+    private JLabel lblFilter;
+    private JPanel jpComponents;
+    private JScrollPane scrpTbl;
+}
