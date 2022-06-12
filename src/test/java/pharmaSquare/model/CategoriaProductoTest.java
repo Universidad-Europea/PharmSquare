40db695a489d6c7b4dd81f26c8fd88c6b5eaa9cc
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-12 17:46:51 +0200

    CategoriaProducto model tested

--- src/test/java/pharmaSquare/model/CategoriaProductoTest.java
+++ src/test/java/pharmaSquare/model/CategoriaProductoTest.java
@@ -1,7 +1,31 @@
 package pharmaSquare.model;
 
+import dam.exception.InvalidDataException;
+import dam.pharmaSquare.model.CategoriaProducto;
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class CategoriaProductoTest {
+
+    private static CategoriaProducto o;
+
+    @BeforeClass
+    public static void setUpBeforeClass() {
+        o = new CategoriaProducto(
+            1,
+            "Test Garcia"
+        );
+    }
+
+    @Test
+    public void idTest() {
+        assertThrows(InvalidDataException.class, () -> {o = new CategoriaProducto(-1, "hola");});
+        assertThrows(InvalidDataException.class, () -> {o = new CategoriaProducto(null, "hola");});
+    }
+
+    @Test
+    public void nombreTest() {
+        assertThrows(InvalidDataException.class, () -> {o = new CategoriaProducto(1, null);});
+        assertThrows(InvalidDataException.class, () -> {o = new CategoriaProducto(1, "");});
+    }
 }
